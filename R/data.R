17f64e35c3f32f4fb1b1f112888c230fd462912e
LiNk-NY <mramos09@gmail.com>
2016-04-25 12:22:39 -0400

    Export and document diseaseCodes dataset

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' TCGA Cancer Disease Codes Table
+#'
+#' A dataset for obtaining the cancer codes in TCGA for about 13 different
+#' types of cancers.
+#'
+#' @format A data frame with 37 rows and 2 variables:
+#' \describe{
+#'      \item{Study.Abbreviation}{Disease Code used in TCGA}
+#'      \item{Study.Name}{The full length study name (i.e., type of cancer)}
+#' }
+#'
+#' @source \url{https://tcga-data.nci.nih.gov/datareports/codeTablesReport.htm}
+"diseaseCodes"
