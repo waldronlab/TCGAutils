80c0974db3848da46512f209d89bc59a2438eaa3
LiNk-NY <mramos09@gmail.com>
2017-12-20 20:26:10 -0500

    add exon download script for reference

---
+++ data-raw/bt.exon_quant.R
@@ -0,0 +1,6 @@
+## Download example dataset from legacy archive
+system("~/src/gdc-client download --manifest ./inst/resources/gdc_manifest_20171221_000206.txt -d ./data-raw/")
+exonFile <- list.files("data-raw", pattern = "cation.txt$", recursive = TRUE, full.names = TRUE)
+exonEx <- read.delim(exonFile, nrows = 100)
+write.table(exonEx, paste0("inst/extdata/", basename(exonFile)), sep = "\t",
+    row.names = FALSE)
