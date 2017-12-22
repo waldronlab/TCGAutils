315629115b5a81280064b0a3f7b3a728d9200d41
LiNk-NY <mramos09@gmail.com>
2017-12-21 20:32:00 -0500

    use dplyr::bind_rows in script

--- data-raw/grlTCGA.R
+++ data-raw/grlTCGA.R
@@ -5,7 +5,7 @@ bl <- getData(blca, "CNASeq")
 blsplit <- lapply(split(bl, bl[["Sample"]]), function(x)
     x[sample(seq_len(nrow(x)), 2L), ])
 
-blframe <- do.call(rbind, blsplit)
+blframe <- dplyr::bind_rows(blsplit)
 blframe <- blframe[c(TRUE, TRUE, FALSE, FALSE), ]
 
 write.table(blframe, file = "inst/extdata/grlTCGA.txt")
