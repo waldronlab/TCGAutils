a83a9af9198f72da1783e38b255c3ad59abc6c05
LiNk-NY <mramos09@gmail.com>
2018-01-02 11:46:58 -0500

    update imports

--- R/TCGAutils-pkg.R
+++ R/TCGAutils-pkg.R
@@ -1,12 +1,13 @@
 #' @import Biobase methods BiocGenerics GenomeInfoDb
 #' @importFrom xml2 read_html
 #' @importFrom rvest html_nodes html_attr
-#' @importFrom GenomicRanges GRanges GRangesList
-#' @importFrom MultiAssayExperiment ExperimentList colData
-#' @importFrom utils data
-#' @importFrom readr read_delim
+#' @importFrom GenomicRanges GRanges GRangesList makeGRangesListFromDataFrame
+#' @importFrom MultiAssayExperiment ExperimentList colData colData<- metadata
+#' @importFrom utils data head read.delim
+#' @importFrom stats as.formula
 #' @importFrom IRanges CharacterList
-#' @importFrom SummarizedExperiment SummarizedExperiment
+#' @importFrom SummarizedExperiment SummarizedExperiment mcols mcols<- rowData
+#' rowData<-
 #' @importFrom GenomicDataCommons files results_all select filter ids
 #' @importFrom S4Vectors isSingleNumber isSingleInteger isSingleString
 #' DataFrame
