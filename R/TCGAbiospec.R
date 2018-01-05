d4ec94f4613d49753e3f5815905c40461fb24ca5
LiNk-NY <mramos09@gmail.com>
2018-01-05 13:31:02 -0500

    update TCGAbiospec function

--- R/TCGAbiospec.R
+++ R/TCGAbiospec.R
@@ -1,3 +1,16 @@
+.strsep <- function(text, pos) {
+    stopifnot(length(unique(nchar(text))) == 1L)
+    lengthText <- unique(nchar(text))
+    allIndx <- seq_len(lengthText)
+    stopifnot(pos %in% allIndx)
+    fgroup <- seq_len(pos)
+    sgroup <- allIndx[!allIndx %in% fgroup]
+    list(
+        substr(text, min(fgroup), max(fgroup)),
+        substr(text, min(sgroup), max(sgroup))
+    )
+}
+
 #' Extract biospecimen data from the TCGA barcode
 #'
 #' This function uses the full TCGA barcode to return a data frame of the
@@ -18,22 +31,39 @@
 TCGAbiospec <- function(barcodes) {
     .checkBarcodes(barcodes)
     filler <- .uniqueDelim(barcodes)
+    maxIndx <- unique(lengths(strsplit(barcodes, filler)))
+    if (maxIndx < 4L)
+        stop("Provide a longer barcode")
+
     local_data_store <- new.env(parent = emptyenv())
     data("sampleTypes", envir = local_data_store)
     sampleTypes <- local_data_store[["sampleTypes"]]
-    sampCode <- substr(TCGAbarcode(barcodes, FALSE, TRUE), 1L, 2L)
-    sample_type <- sampleTypes[["Definition"]][
-        match(sampCode, sampleTypes[["Code"]])]
-    newBiospec <-
+    sampCode <- TCGAbarcode(barcodes, FALSE, TRUE)
+    sampVial <- .strsep(sampCode, 2L)
+    names(sampVial) <- c("sample", "vial")
+    sample_definition <- sampleTypes[["Definition"]][
+        match(sampVial[["sample"]], sampleTypes[["Code"]])]
+    biospec <-
         data.frame(
-            patient_id = TCGAbarcode(barcodes),
-            sample_type,
-            sample_code = TCGAbarcode(barcodes, FALSE, TRUE),
+            submitter_id = TCGAbarcode(barcodes),
+            sample_definition,
+            as.data.frame(sampVial, stringsAsFactors = FALSE),
             stringsAsFactors = FALSE
         )
+    if (maxIndx == 4L)
+        return(biospec)
+    else
+        splitDex <- seq(5L, maxIndx)
+
+    tailBarcode <- strsplit(TCGAbarcode(barcodes, index = splitDex), filler)
+    splitCol <- splitDex == 5L
+    tailBarcode <- lapply(tailBarcode, function(x)
+        c(unlist(.strsep(x[[1L]], 2L)), x[!splitCol]))
+
     portPlateCent <- do.call(rbind.data.frame,
-        args = c(strsplit(TCGAbarcode(barcodes, index = 5L:7L), filler),
-            list(stringsAsFactors = FALSE)))
-    names(portPlateCent) <- c("portion", "plate", "center")
-    cbind.data.frame(newBiospec, portPlateCent, stringsAsFactors = FALSE)
+        args = c(tailBarcode, list(stringsAsFactors = FALSE)))
+    names(portPlateCent) <-
+        c("portion", "analyte", "plate", "center")[seq_along(c(splitDex, 1L))]
+
+    cbind.data.frame(biospec, portPlateCent, stringsAsFactors = FALSE)
 }
