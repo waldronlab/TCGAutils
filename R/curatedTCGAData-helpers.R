7e31751d8b7d3f726d6eafd35d726c9e66d20455
LiNk-NY <mramos09@gmail.com>
2018-01-10 18:00:41 -0500

    add return values to docs for helpers

--- R/curatedTCGAData-helpers.R
+++ R/curatedTCGAData-helpers.R
@@ -5,24 +5,53 @@
 #' @title Helper functions for managing MultiAssayExperiment from
 #' curatedTCGAData
 #'
-#' @description
-#' Additional helper functions for cleaning and uncovering metadata
-#' within a downloaded \code{MultiAssayExperiment} from \code{curatedTCGAData}
-#'
 #' @aliases getSubtypeMap
 #'
+#' @description
+#' Additional helper functions for cleaning and uncovering metadata
+#' within a downloaded \code{MultiAssayExperiment} from \code{curatedTCGAData}.
+#' The \code{getSubtypeMap} function provides a 2 column \code{data.frame}
+#' with in-data variable names and an interpreted names. The
+#' \code{getClinicalNames} function provides a vector of variable names that
+#' exist in the \code{colData} slot of a downloaded \code{MultiAssayExperiment}
+#' object. These variables are obtained from
+#' \link[RTCGAToolbox]{getFirehoseData} by default and tend to be present
+#' across most cancer codes.
+#'
 #' @param multiassayexperiment A \linkS4class{MultiAssayExperiment} object
+#'
+#' @examples
+#' \dontrun{
+#' library(curatedTCGAData)
+#'
+#' coad <- curatedTCGAData(diseaseCode = "COAD",
+#'     assays = "CNA*", dry.run = FALSE)
+#' getSubtypeMap(coad)
+#' }
+#'
+#' @return \itemize{
+#'     \item{getSubtypeMap}: A \code{data.frame} with columns representing
+#'     actual data variables and explanatory names
+#'     \item{getClinicalNames}: A \code{vector} of names that correspond to
+#'     a particular disease code.
+#' }
+#'
 #' @export
 getSubtypeMap <- function(multiassayexperiment) {
+    if (!is(multiassayexperiment, "MultiAssayExperiment"))
+        stop("Provide a 'MultiAssayExperiment' object")
     frameMap <- metadata(colData(multiassayexperiment))[["subtypes"]]
     if (is.null(frameMap)) {
-        message("No data available")
+        message("No subtype data available")
     } else frameMap
 }
 
 #' @rdname curatedTCGAData-helpers
 #'
 #' @param diseaseCode A TCGA cancer code (e.g., "BRCA")
+#' @examples
+#' getClinicalNames("COAD")
+#'
 #' @export
 getClinicalNames <- function(diseaseCode) {
     stopifnot(S4Vectors::isSingleString(diseaseCode))
