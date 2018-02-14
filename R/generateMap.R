7c3785e5c886d7103fecbc29a498828939ab6d38
LiNk-NY <mramos09@gmail.com>
2018-02-14 18:48:44 -0500

    update generateMap functionality

--- R/generateMap.R
+++ R/generateMap.R
@@ -13,6 +13,10 @@
 #' @param idConverter A function to be used against the sample or specimen
 #' identifiers to match those in the rownames of the \code{colData}
 #' (default NULL)
+#' @param sampleCol A single string indicating the sample identifiers
+#' column in the colData dataset
+#' @param patientCol A single string indicating the patient identifiers
+#' in colData, "row.names" extracts the colData row names
 #' @param ... Additonal arguments to pass to the 'idConverter' function.
 #'
 #' @return A \code{DataFrame} class object of mapped samples and patient
@@ -27,33 +31,48 @@
 #'     assay2 = matrix(1:4, ncol = 2,
 #'         dimnames = list(paste0("gene", 1:2), c("A-L", "B-L"))))
 #'
-#' ## A ficticious colData
+#' ## Mock colData
 #' myPheno <- data.frame(var1 = c("Yes", "No"), var2 = c("High", "Low"),
 #'     row.names = c("a", "b"))
 #'
-#' ## Provide an idConverter function to take corresponding letter codes
-#' ## for each biological unit
+#' ## A look at the identifiers
+#' vapply(expList, colnames, character(2L))
+#' rownames(myPheno)
+#'
+#' ## Use 'idConverter' to correspond sample names to patient identifiers
 #' generateMap(expList, myPheno,
 #'     idConverter = function(x) substr(tolower(x), 1L, 1L))
 #'
 #' @export generateMap
-generateMap <- function(experiments, colData, idConverter = NULL, ...) {
+generateMap <- function(experiments, colData, idConverter = identity,
+    sampleCol, patientCol, ...) {
     if (!is(experiments, "ExperimentList"))
-    experiments <- ExperimentList(experiments)
+        experiments <- ExperimentList(experiments)
     samps <- colnames(experiments)
-    assay <- factor(rep(names(samps), lengths(samps)), levels=names(samps))
+    expnames <- names(samps)
+    assay <- factor(rep(expnames, lengths(samps)), levels=expnames)
     colname <- unlist(samps, use.names=FALSE)
-    if (is.null(idConverter)) {
-        matches <- match(colname, rownames(colData))
+    if (!missing(sampleCol) && !missing(patientCol)) {
+        if (!S4Vectors::isSingleString(sampleCol) ||
+            !S4Vectors::isSingleString(patientCol))
+            stop("Provide character names in colData for mapping")
+        pts <- if (patientCol == "row.names") rownames(colData)
+            else colData[[patientCol]]
+        samples <- colData[[sampleCol]]
+        autoMap <- cbind.data.frame(assay = NA_character_, primary = pts,
+            colname = samples, stringsAsFactors = FALSE)
+        for (i in expnames) {
+            autoMap[samps[[i]] %in% samples, "assay"] <- i
+        }
+        autoMap[, "assay"] <- factor(autoMap[["assay"]])
     } else {
         matches <- match(idConverter(colname, ...), rownames(colData))
+        if (length(matches) && all(is.na(matches)))
+            stop("no way to map colData to ExperimentList")
+        primary <- rownames(colData)[matches]
+        autoMap <- S4Vectors::DataFrame(assay=assay,
+            primary=primary, colname=colname)
     }
-    if (length(matches) && all(is.na(matches)))
-        stop("no way to map colData to ExperimentList")
-    primary <- rownames(colData)[matches]
-    autoMap <- S4Vectors::DataFrame(assay=assay,
-                                    primary=primary,
-                                    colname=colname)
     missingPrimary <- is.na(autoMap[["primary"]])
     if (nrow(autoMap) && any(missingPrimary)) {
         notFound <- autoMap[missingPrimary, ]
@@ -64,3 +83,4 @@ generateMap <- function(experiments, colData, idConverter = NULL, ...) {
     }
     autoMap
 }
+
