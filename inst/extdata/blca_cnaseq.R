7db35438d887a78620724d7c38e6a1ce8d310c8f
LiNk-NY <mramos09@gmail.com>
2018-04-11 16:09:13 -0400

    update script for consistent blca_cnaseq output

--- inst/extdata/blca_cnaseq.R
+++ inst/extdata/blca_cnaseq.R
@@ -2,13 +2,17 @@
 if (!requireNamespace("RTCGAToolbox"))
     stop("Download package 'RTCGAToolbox' to regenerate data")
 
-blca <- RTCGAToolbox::getFirehoseData("BLCA", clinical = FALSE,
+library(RTCGAToolbox)
+
+blca <- getFirehoseData("BLCA", clinical = FALSE,
     CNASeq = TRUE, destdir = tempdir())
-bl <- RTCGAToolbox::getData(blca, "CNASeq")
+bl <- getData(blca, "CNASeq")
+
+set.seed(777)
 blsplit <- lapply(split(bl, bl[["Sample"]]), function(x)
     x[sample(seq_len(nrow(x)), 2L), ])
 
 blframe <- dplyr::bind_rows(blsplit)
 blca_cnaseq <- blframe[c(TRUE, TRUE, FALSE, FALSE), ]
 
-write.table(blca_cnaseq, file = "blca_cnaseq.txt")
+write.table(blca_cnaseq, file = "inst/extdata/blca_cnaseq.txt")
