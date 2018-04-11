a2fa60b1ca4e2efee4f353844e084dab4e5f4005
LiNk-NY <mramos09@gmail.com>
2018-04-11 15:53:07 -0400

    update exon_quant script

--- inst/extdata/bt.exon_quant.R
+++ inst/extdata/bt.exon_quant.R
@@ -2,23 +2,22 @@
 if (!requireNamespace("GenomicDataCommons"))
     stop("Please download 'GenomicDataCommons' to update file")
 
+library(GenomicDataCommons)
+
 manifile <- files(legacy = TRUE) %>%
     filter(~ file_id == "d56a5dec-cb55-457f-8d93-dd1f3911ae9f") %>%
         manifest()
 
-write.table(manifile, file = "inst/resources/gdc_manifest_20171221_000206.txt")
+gdcdata(manifile[["id"]], use_cached = TRUE)
 
-gdcdata(manifile[["id"]], destination_dir = "inst/extdata",
-    overwrite = TRUE, progress = FALSE)
-
-flist <- list.files("inst/extdata", pattern = "cation.txt$",
-    recursive = TRUE, full.names = TRUE)
+flist <- list.files(gdc_cache(), pattern = "cation.txt$", recursive = TRUE,
+    full.names = TRUE)
 flist <- flist[grepl("^unc", basename(flist))]
 
-exonFile <- "inst/extdata/bt.exon_quantification.txt"
+exonFile <- "bt.exon_quantification.txt"
 file.rename(flist, exonFile)
 
 exonEx <- read.delim(exonFile, nrows = 100)
 
-write.table(exonEx, paste0("inst/extdata/", basename(exonFile)), sep = "\t",
-    row.names = FALSE)
+write.table(exonEx, file.path("inst", "extdata", basename(exonFile)),
+    sep = "\t", row.names = FALSE)
