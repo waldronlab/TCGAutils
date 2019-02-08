c2be9e1a2d10af4be4ecc47676d7d5f3c16daeb5
LiNk-NY <mramos09@gmail.com>
2019-02-08 14:21:59 -0500

    TCGAsampleSelect works with vector of sample codes

--- R/TCGAsampleSelect.R
+++ R/TCGAsampleSelect.R
@@ -7,22 +7,21 @@
 #'
 #' @param barcodes Standard TCGA barcodes containing patient identifiers,
 #' sample, portion, plate, center codes.
-#' @param sampleCode Either a character or numeric vector of length one. See
-#' the \code{sampleType} dataset.
+#' @param sampleCodes Either a character or numeric vector of TCGA sample codes.
+#' See the \code{sampleType} dataset.
 #'
 #' @return A logical vector of the same length as 'barcodes' indicating matches
 #'
 #' @examples
 #'
 #' example("TCGAbarcode")
-#' TCGAsampleSelect(barcodes, 11)
+#' TCGAsampleSelect(barcodes, c(11, 01))
 #'
 #' @export TCGAsampleSelect
-TCGAsampleSelect <- function(barcodes, sampleCode) {
+TCGAsampleSelect <- function(barcodes, sampleCodes) {
     stopifnot(
-        is.character(sampleCode) || is.numeric(sampleCode),
-        identical(length(sampleCode), 1L),
-        !is.na(sampleCode), !is.logical(sampleCode)
+        is.character(sampleCodes) || is.numeric(sampleCodes),
+        !is.na(sampleCodes), !is.logical(sampleCodes)
     )
     bcodeCharLen <- unique(nchar(barcodes))
     if (!S4Vectors::isSingleNumber(bcodeCharLen))
@@ -34,17 +33,15 @@ TCGAsampleSelect <- function(barcodes, sampleCode) {
     local_data_store <- new.env(parent = emptyenv())
     data("sampleTypes", envir = local_data_store, package = "TCGAutils")
     sampleTypes <- local_data_store[["sampleTypes"]]
-    if (is.numeric(sampleCode)) {
-        if (identical(nchar(as.character(sampleCode)), 1L))
-            sampleCode <- paste0("0", sampleCode)
-    }
-    if (!sampleCode %in% sampleTypes[["Code"]])
-        stop("'sampleCode' not in look up table")
-    LongName <- as.character(
-        sampleTypes[match(sampleCode, sampleTypes[["Code"]]), "Definition"]
-    )
-    message("Selecting '", LongName, "' samples")
+
+    ncodes <- nchar(as.character(sampleCodes))
+    singles <- ncodes == 1L
+    sampleCodes[singles] <- paste0("0", sampleCodes[singles])
+
+    if (any(!sampleCodes %in% sampleTypes[["Code"]]))
+        stop("At least one sample code not found in look-up table")
+
     sampleSnippet <- TCGAbarcode(barcodes, sample = TRUE, participant = FALSE)
     barcodeSamples <- substr(sampleSnippet, 1L, 2L)
-    return(barcodeSamples == sampleCode)
+    return(setNames(barcodeSamples %in% sampleCodes, barcodeSamples))
 }
