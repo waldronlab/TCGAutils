45c8cacccc14200721695fe4ab25cc4e398d29b2
LiNk-NY <mramos09@gmail.com>
2019-03-19 18:34:58 -0400

    add trimColData function, update examples, include pkgs in Suggests

--- R/simplifyColData.R
+++ R/simplifyColData.R
@@ -35,3 +35,44 @@ mergeColData <- function(MultiAssayExperiment, colData) {
     colData(MultiAssayExperiment) <- as(mergedClin, "DataFrame")
     MultiAssayExperiment
 }
+
+#' Minimize the number of variables in colData
+#'
+#' This function removes variables that have a high number of missing data
+#' and contain keywords.
+#'
+#' @param multiassayexperiment A \linkS4class{MultiAssayExperiment} object with colData
+#' @param maxNAfrac (numeric default 0.2) A decimal between 0 and 1 to indicate
+#' the amount of NA values allowed per column
+#' @param keystring (character) A vector of keywords to match and remove
+#' variables
+#'
+#' @return A \code{\linkS4class{MultiAssayExperiment}} object
+#'
+#' @examples
+#'
+#' library(curatedTCGAData)
+#'
+#' gbm <- curatedTCGAData("GBM", "CNASNP", FALSE)
+#' (gbm_trimmed <- trimColData(gbm))
+#' head(colData(gbm_trimmed))[1:5]
+#'
+#' @export trimColData
+trimColData <- function(multiassayexperiment, maxNAfrac = 0.2,
+    keystring = c("portion", "analyte")) {
+    if (!is(multiassayexperiment, "MultiAssayExperiment"))
+        stop("Provide a 'MultiAssayExperiment' input")
+    DF <- colData(multiassayexperiment)
+    keystring <- na.omit(keystring)
+
+    NAabove <- vapply(DF, function(x) mean(is.na(x)) >= maxNAfrac, logical(1L))
+
+    keymat <- vapply(keystring, function(string)
+        grepl(string, names(DF)), logical(length(DF)))
+    keymatch <- apply(keymat, 1L, any)
+
+    todrop <- NAabove | keymatch
+    colData(multiassayexperiment) <- DF[, !todrop]
+
+    multiassayexperiment
+}
