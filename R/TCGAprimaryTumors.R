e3484f44d91131a602e9d2954e0cd292719c3c77
LiNk-NY <mramos09@gmail.com>
2019-09-17 15:15:09 -0400

    include helper function to select primary tumors from TCGA data

---
+++ R/TCGAprimaryTumors.R
@@ -0,0 +1,27 @@
+#' Select primary tumors from TCGA datasets
+#'
+#' Tumor selection is decided using the `sampleTypes` data. For 'LAML' datasets,
+#' the primary tumor code used is "03" otherwise, "01" is used.
+#'
+#' @param multiassayexperiment A \linkS4class{MultiAssayExperiment} with TCGA
+#'     data as obtained from \code{\link[curatedTCGAData]{curatedTCGAData}}
+#'
+#' @return A MultiAssayExperiment containing only primary tumor samples
+#'
+#' @export TCGAprimaryTumors
+TCGAprimaryTumors <- function(multiassayexperiment) {
+    if (!is(multiassayexperiment, "MultiAssayExperiment"))
+        stop("Provide a 'MultiAssayExperiment' object as input")
+
+    exptnames <- names(experiments(multiassayexperiment))
+    dcodes <- vapply(strsplit(exptnames, "_"), `[[`, character(1L), 1L)
+
+    primaries <- ifelse(dcodes == "LAML", "03", "01")
+    primaries <- setNames(primaries, dcodes)
+
+    logisub <- Map(function(barcodes, tumorcode) {
+        TCGAsampleSelect(barcodes, tumorcode)
+    }, colnames(multiassayexperiment), primaries)
+
+    multiassayexperiment[, logisub, ]
+}
