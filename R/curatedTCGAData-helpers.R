52b9a9bcb1aa149635b0f56cbb301cfc5f5e6685
LiNk-NY <mramos09@gmail.com>
2020-12-09 11:28:45 -0500

    update curatedTCGAData code

--- R/curatedTCGAData-helpers.R
+++ R/curatedTCGAData-helpers.R
@@ -43,7 +43,7 @@ NULL
 #'
 #' library(curatedTCGAData)
 #'
-#' gbm <- curatedTCGAData("GBM", c("RPPA*", "CNA*"), FALSE)
+#' gbm <- curatedTCGAData("GBM", c("RPPA*", "CNA*"), version = "2.0.0", FALSE)
 #'
 #' getSubtypeMap(gbm)
 #'
