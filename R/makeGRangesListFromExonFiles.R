031133172b59017eaa6e292ec5cacf7850bddde5
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-02-28 13:02:21 -0500

    revamp makeGRangesListFromExonFiles

--- R/makeGRangesListFromExonFiles.R
+++ R/makeGRangesListFromExonFiles.R
@@ -1,23 +1,38 @@
-#' Read Exon level files and create a GRangesList
+#' Read exon-level expression files and create a `GRangesList`
 #'
 #' This function serves to read exon-level expression data. It works for exon
 #' quantification (raw counts and RPKM) and junction quantification
-#' (raw counts) files paths and represent such data as a
-#' \linkS4class{GRangesList}. The data can be downloaded
-#' via the TCGA Legacy Archive. File name and structure requirements are as
-#' follows: The third position delimited by dots (".") in the file name should
-#' be the universally unique identifier (UUID). The column containing the
-#' ranged information is labeled "exon."
+#' (raw counts) file paths and represents such data as a
+#' \linkS4class{GRangesList}. The data files can be downloaded
+#' via the Genomic Data Commons (GDC) Legacy Archive.
 #'
-#' @param filepaths A \code{character} vector of valid exon data file paths
-#' @param sampleNames A \code{character} vector of TCGA barcodes to be applied
-#' if not present in the data (default NULL)
-#' @param fileNames A \code{character} vector of file names as downloaded from
-#' the Genomic Data Commons Legacy archive (default NULL)
-#' @param rangesColumn (default "exon") A single string indicating the name of
-#' the column in the data containing the ranges information
-#' @param nrows The number of rows to return from each of the files read in
-#' (all rows by default)
+#' @details The `rangesColumn` name in the GDC data files is usually "exon"
+#'   but can be changed with the `rangesColumn` argument, if different.
+#'   To avoid programmatically obtaining TCGA barcodes from the GDC
+#'   API, set the `getBarcodes` to `FALSE`. When `getBarcodes` is set to
+#'   `FALSE`, the file names are used to name the elements of the `GRangesList`
+#'   output.
+#'
+#' @param filepaths character() vector of file paths containing TCGA exon
+#'     data usually obtained from the GDC
+#'
+#' @param sampleNames character() vector of TCGA barcodes to be used as
+#'     names for the `GRangesList` output (default NULL)
+#'
+#' @param fileNames character() vector of file names as downloaded from
+#'     the Genomic Data Commons Legacy archive (default `basename(filepaths)`)
+#'
+#' @param getBarcodes logical(1). Whether to query the GDC API with the
+#'     `filenameToBarcode` and obtain the TCGA barcodes from the file names
+#'     (default TRUE); see details.
+#'
+#' @param rangesColumn character(1). The name of the column in the data
+#'     containing the ranges information (default "exon"); see details.
+#'
+#' @param nrows numeric(1). The number of rows to return from each of the files
+#'     read in (all rows by default; default Inf)
+#'
+#' @md
 #'
 #' @return A \linkS4class{GRangesList} object
 #'
@@ -38,17 +53,20 @@
 #'
 #' @export makeGRangesListFromExonFiles
 makeGRangesListFromExonFiles <- function(filepaths, sampleNames = NULL,
-    fileNames = NULL, rangesColumn = "exon", nrows = Inf)
+    fileNames = basename(filepaths), getBarcodes = TRUE, rangesColumn = "exon",
+    nrows = Inf)
 {
-    if (!is.null(sampleNames)) {
-        if (length(filepaths) != length(sampleNames))
-            stop("Inconsistent sample names obtained from file names")
-    } else {
-        queryNames <-
-            if (!is.null(fileNames)) fileNames else basename(filepaths)
+    if (is.null(sampleNames) && getBarcodes) {
         sampleNames <-
-            filenameToBarcode(queryNames, TRUE)[["aliquots.submitter_id"]]
+            filenameToBarcode(filenames = fileNames, legacy = TRUE)[[
+                "cases.samples.portions.analytes.aliquots.submitter_id"
+            ]]
+    } else if (is.null(sampleNames)) {
+        sampleNames <- fileNames
     }
+
+    if (!identical(length(filepaths), length(sampleNames)))
+        stop("'sampleNames' length is inconsistent with 'fileNames'")
 
     btData <- lapply(filepaths, function(file) {
         if (requireNamespace("readr", quietly = TRUE))
@@ -58,16 +76,13 @@ makeGRangesListFromExonFiles <- function(filepaths, sampleNames = NULL,
                 nrows = if (is.infinite(nrows)) -1 else nrows)
     })
 
-    if (!length(sampleNames))
-        sampleNames <- NULL
-
     names(btData) <- sampleNames
 
-    allrowdata <- if (requireNamespace("dplyr", quietly = TRUE))
-        dplyr::bind_rows(btData)
-    else
-        do.call(rbind, btData)
-
+    allrowdata <-
+        if (requireNamespace("dplyr", quietly = TRUE))
+            dplyr::bind_rows(btData)
+        else
+            do.call(rbind, btData)
 
     newGRanges <- GenomicRanges::GRanges(allrowdata[[rangesColumn]])
     mcols(newGRanges) <- allrowdata[, names(allrowdata) != rangesColumn]
