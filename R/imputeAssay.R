bba797b48b4054deae1c52ac87b60c866359b143
LiNk-NY <marcel.ramos@sph.cuny.edu>
2026-05-15 16:02:01 -0400

    minor formatting

--- R/imputeAssay.R
+++ R/imputeAssay.R
@@ -6,10 +6,12 @@
 #' @description These function allow the user to enter a
 #' `MultiAssayExperiment` and impute all the NA values inside assays.
 #'
-#' @param multiassayexperiment A `MultiAssayExperiment` with genes in the
-#' rows, samples in the columns
-#' @param i A numeric, logical, or character `vector` indicating the
-#' assays to perform imputation on (default 1L)
+#' @param multiassayexperiment A `MultiAssayExperiment` with genes in the rows,
+#'   samples in the columns
+#'
+#' @param i A numeric, logical, or character `vector` indicating the assays to
+#'   perform imputation on (default 1L)
+#'
 #' @inheritDotParams impute::impute.knn
 #'
 #' @return A `MultiAssayExperiment` with imputed assays values
@@ -34,7 +36,7 @@ imputeAssay <- function(multiassayexperiment, i = 1, ...) {
     if (!any(is.character(i), is.numeric(i), is.logical(i)))
         stop("'i' has to be character or numeric or logical")
 
-    sub.multiassayexperiment <- multiassayexperiment[,,i]
+    sub.multiassayexperiment <- multiassayexperiment[, , i]
     assays <- assays(sub.multiassayexperiment)
     assayclasses <- vapply(assays, is.matrix, logical(1L))
     if (!all(assayclasses))
